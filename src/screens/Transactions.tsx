584ed300ed9a26e20417e2b1552b956a1a6cd95a
Pedro Furlan <pedrofurlan6@gmail.com>
2023-08-25 20:38:40 -0300

    Creating a bottom sheet to create a new transaction

--- src/screens/Transactions.tsx
+++ src/screens/Transactions.tsx
@@ -1,12 +1,16 @@
-import { Box, FlatList, HStack, ScrollView, Text, VStack } from "native-base";
+import { Box, FlatList, HStack, ScrollView, Text, VStack, useDisclose } from "native-base";
 import { MainHeader } from "../components/MainHeader";
 import { TransactionCard } from "../components/TransactionCard";
 import { Button } from "../components/Button";
 import { TransactionDetails } from "../components/TransactionDetails";
+import { Actionsheet } from 'native-base'
+import { Feather } from '@expo/vector-icons'
 
 const fakeData = ['Teste 1', 'Teste 2', 'Teste 3', 'Teste 4', 'Teste 5']
 
 export function Transactions() {
+  const { isOpen, onOpen, onClose } = useDisclose();
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -33,7 +37,21 @@ export function Transactions() {
             title="Nova transação"
             backgroundColor="#fbbf24"
             textColor="white"
+            onPress={onOpen}
           />
+
+          <Actionsheet isOpen={isOpen} onClose={onClose}>
+            <Actionsheet.Content>
+              <VStack
+                p={8}
+              >
+                <HStack display="flex" alignItems="center" justifyContent="space-between">
+                  <Text color="gray.200" bold fontSize="md">Nova transação</Text>
+                  <Feather name="x" size={12} color="#ef4444" onPress={onClose} />
+                </HStack>
+              </VStack>
+            </Actionsheet.Content>
+          </Actionsheet>
         </VStack>
         <TransactionCard title="Total de gastos" amount={1200} mb={4} />
         <TransactionCard title="Balanço geral" amount={1400} mb={4} />
